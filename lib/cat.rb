9add4bd0a2cc86a24b7fb41012e17210a51d9184
Amanda Rolon <abrolon87@gmail.com>
2020-01-05 20:50:27 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,5 +1,4 @@
 class Cat
-<<<<<<< HEAD
 
   attr_accessor :owner, :mood
   attr_reader :name
@@ -16,13 +15,13 @@ class Cat
   def self.all
     @@all
   end
-=======
-  attr_reader :name
-  attr_writer :owner
 
-  def initialize(name, owner)
-  @name = name
-  @owner = owner
-  end# code goes here
->>>>>>> 8e1e2398b8e89db95ff682f5ae1fa398c66e445f
+  # attr_reader :name
+  # attr_writer :owner
+  #
+  # def initialize(name, owner)
+  # @name = name
+  # @owner = owner
+  # end# code goes here
+
 end
